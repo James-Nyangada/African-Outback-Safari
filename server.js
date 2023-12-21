34093b1b39b7f8d3faf57ab71abd2d268a773b6d
IsoDevMate <oumaoduor5827@gmail.com>
2023-12-21 22:40:51 +0300

    added server.js functionality

--- server.js
+++ server.js
@@ -1,36 +1,53 @@
 const express = require('express');
-const axios = require('axios');
 const bodyParser = require('body-parser');
+const request = require('superagent');
 
 const app = express();
 const port = 3001;
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
+const mailchimpInstance = 'us21';
+const listUniqueId = '8444b6a9e9';
+const mailchimpApiKey = 'ebab15097cd01a4f1922270c2a2ab76b-us21';
 app.post('/subscribe', async (req, res) => {
-  const { email } = req.body;
+  const { email_address } = req.body;
+  console.log(req.body);
 
   try {
-    const response = await axios.post(
-      'https://us17.api.mailchimp.com/3.0/lists/a2b81893f8/members',
-      {
-        email_address: email,
-        status: 'subscribed',
-      },
-      {
-        headers: {
-          Authorization: 'apikey 5d8e4396cead2fdcf5121aa01cfbadee-us17',
-        },
-      }
-    );
+    // Check if member already exists
+    const memberResponse = await request
+      .get(`https://${mailchimpInstance}.api.mailchimp.com/3.0/lists/${listUniqueId}/members/${email_address}`)
+      .set('Content-Type', 'application/json;charset=utf-8')
+      .set('Authorization', `Basic ${Buffer.from(`any:${mailchimpApiKey}`).toString('base64')}`);
 
-    console.log(response.data);
+    if (memberResponse.body.status === 'subscribed') {
+      res.status(400).json({ success: false, message: 'Member already exists please try another email' });
+      return;
+    }
+  } catch (error) {
+    console.error(error.response.body);
+  }
+
+  try {
+    const response = await request
+      .post(`https://${mailchimpInstance}.api.mailchimp.com/3.0/lists/${listUniqueId}/members/`)
+      .set('Content-Type', 'application/json;charset=utf-8')
+      .set('Authorization', `Basic ${Buffer.from(`any:${mailchimpApiKey}`).toString('base64')}`)
+      .send({
+        email_address: email_address,
+        status: 'subscribed',
+      });
+
+    console.log(response.body);
     res.status(200).json({ success: true, message: 'Subscription successful' });
   } catch (error) {
-    console.error(error.response.data);
+    console.error(error.response.body);
     res.status(500).json({ success: false, message: 'Subscription failed' });
   }
 });
+
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
