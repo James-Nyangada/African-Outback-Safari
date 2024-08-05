190bc4cf0c07ee5639baab928cb67a8a4abeb767
James Nyangada <jamesedwin03@gmail.com>
2024-08-05 10:57:22 +0300

    admin dashboard

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import { useEffect } from "react";
 import Navbar from "./components/Navbar";
 import Home from "./routes/Home";
-import Blog from  "./routes/Blog"
+import Blog from "./routes/Blog";
 import SantoriniPage from "./routes/SantoriniPage";
 import Destinations from "./routes/Destinations";
 import HolidayTypes from "./routes/HolidayTypes";
@@ -17,41 +17,49 @@ import MountKenyaPage from "./routes/MountKenyaPage";
 import MaasaiMaraPage from "./routes/MaasaiMaraPage";
 import AmboseliPage from "./routes/AmboseliPage";
 import LakeNakuruPage from "./routes/LakeNakuruPage";
-import SamburuPage from "./routes/SamburuPage"
-import MountKilimanjaroPage from "./routes/MountKilimanjaroPage"
-import SerengetiPage from "./routes/SerengetiPage"
-import NgorongoroPage from "./routes/NgorongoroPage"
-import LakeManyaraPage from "./routes/LakeManyaraPage"
+import SamburuPage from "./routes/SamburuPage";
+import MountKilimanjaroPage from "./routes/MountKilimanjaroPage";
+import SerengetiPage from "./routes/SerengetiPage";
+import NgorongoroPage from "./routes/NgorongoroPage";
+import LakeManyaraPage from "./routes/LakeManyaraPage";
 
 import Samburu from "./routes/Trip destination details/Samburu";
-import Amboseli from "./routes/Trip destination details/Amboseli"
-import Chogoria from "./routes/Trip destination details/Chogoria"
-import HikingChogoria from "./routes/Trip destination details/HikingChogoria"
-import LakeNakuru from "./routes/Trip destination details/LakeNakuru"
-import MaasaiMara from "./routes/Trip destination details/MaasaiMara"
-import NaroMoru from "./routes/Trip destination details/NaroMoru"
-import SirimonChogoria from "./routes/Trip destination details/SimironChogoria"
-import SirimonFourDay from "./routes/Trip destination details/SirimonFourDay"
-import SirimonSixDay from "./routes/Trip destination details/SirimonSixDay"
+import Amboseli from "./routes/Trip destination details/Amboseli";
+import Chogoria from "./routes/Trip destination details/Chogoria";
+import HikingChogoria from "./routes/Trip destination details/HikingChogoria";
+import LakeNakuru from "./routes/Trip destination details/LakeNakuru";
+import MaasaiMara from "./routes/Trip destination details/MaasaiMara";
+import NaroMoru from "./routes/Trip destination details/NaroMoru";
+import SirimonChogoria from "./routes/Trip destination details/SimironChogoria";
+import SirimonFourDay from "./routes/Trip destination details/SirimonFourDay";
+import SirimonSixDay from "./routes/Trip destination details/SirimonSixDay";
+import Itinerary from "./routes/Itinerary.jsx";
+import Admin from "./routes/Admin";
+import AdminLayout from "./AdminLayout";
+import { TooltipProvider } from "../src/components/ui/tooltip"; // Adjust the import path based on your project structure
 
 function App() {
+  const location = useLocation();
+
   // Scroll to the top when the route changes
   useEffect(() => {
     window.scrollTo(0, 0);
-  }, []);
+  }, [location.pathname]);
 
   return (
     <div className="App">
-      <Navbar />
+     <TooltipProvider>
+      {location.pathname !== "/admin-dashboard" && <Navbar />}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/destinations" element={<Destinations />} />
         <Route path="/about-us" element={<AboutUs />} />
         <Route path="/holiday-types" element={<HolidayTypes />} />
-        <Route path="/blog" element={<Blog/>} />
+        <Route path="/blog" element={<Blog />} />
         <Route path="/contact-us" element={<ContactUs />} />
         <Route path="/book-now" element={<BookNow />} />
         <Route path="/log-in" element={<LogIn />} />
+        <Route path="/create-booking" element={<Itinerary />} />
 
         <Route path="/santorini" element={<SantoriniPage />} />
         <Route path="/mount-kenya" element={<MountKenyaPage />} />
@@ -59,33 +67,35 @@ function App() {
         <Route path="/amboseli" element={<AmboseliPage />} />
         <Route path="/lake-nakuru" element={<LakeNakuruPage />} />
         <Route path="/samburu" element={<SamburuPage />} />
-        <Route path="/mount-kilimanjaro" element={<MountKilimanjaroPage/>} />
-        <Route path="/serengeti" element={<SerengetiPage/>} />
-        <Route path="/ngorongoro" element={<NgorongoroPage/>} />
-        <Route path="/lake-manyara" element={<LakeManyaraPage/>} />
+        <Route path="/mount-kilimanjaro" element={<MountKilimanjaroPage />} />
+        <Route path="/serengeti" element={<SerengetiPage />} />
+        <Route path="/ngorongoro" element={<NgorongoroPage />} />
+        <Route path="/lake-manyara" element={<LakeManyaraPage />} />
 
-
-        {/* Trip destionation details */}
-        
-        <Route path="/samburu-info" element={<Samburu/>} />
-        <Route path="/amboseli-info" element={<Amboseli/>} />
-        <Route path="/chogoria-info" element={<Chogoria/>} />
-        <Route path="/hiking-chogoria-info" element={<HikingChogoria/>} />
-        <Route path="/lake-nakuru-info" element={<LakeNakuru/>} />
-        <Route path="/maasai-mara-info" element={<MaasaiMara/>} />
-        <Route path="/naro-moru-info" element={<NaroMoru/>} />
-        <Route path="/sirimon-chogoria-info" element={<SirimonChogoria/>} />
-        <Route path="/sirimon-four-day-info" element={<SirimonFourDay/>} />
-        <Route path="/sirimon-six-day-info" element={<SirimonSixDay/>} />
-        
+        {/* Trip destination details */}
+        <Route path="/samburu-info" element={<Samburu />} />
+        <Route path="/amboseli-info" element={<Amboseli />} />
+        <Route path="/chogoria-info" element={<Chogoria />} />
+        <Route path="/hiking-chogoria-info" element={<HikingChogoria />} />
+        <Route path="/lake-nakuru-info" element={<LakeNakuru />} />
+        <Route path="/maasai-mara-info" element={<MaasaiMara />} />
+        <Route path="/naro-moru-info" element={<NaroMoru />} />
+        <Route path="/sirimon-chogoria-info" element={<SirimonChogoria />} />
+        <Route path="/sirimon-four-day-info" element={<SirimonFourDay />} />
+        <Route path="/sirimon-six-day-info" element={<SirimonSixDay />} />
 
         <Route path="/anteliz-suites" element={<AntelizHotel />} />
         <Route
           path="/anteliz-suites/anteliz-private-villa"
           element={<AntelizPrivateVilla />}
         />
+        <Route
+          path="/admin-dashboard"
+          element={<AdminLayout><Admin /></AdminLayout>}
+        />
       </Routes>
-      <Footer />
+      {location.pathname !== "/admin-dashboard" && <Footer />}
+      </TooltipProvider>
     </div>
   );
 }
---
+++ src/routes/Itinerary.jsx
@@ -0,0 +1,229 @@
+import axios from "axios";
+import { useState } from "react";
+import styled from "styled-components";
+
+const Itinerary = () =>{
+    const [tripDetails, setTripDetails] = useState({
+        name: "",
+        emailaddress: "",
+        phonenumber: "",
+        address: "",
+        city: "",
+        nationality: "",
+        touristattraction: "",
+        activities: "",
+        hotel: "",
+        month: "",
+        year: "",
+        days: "",
+        tourcategory: "",
+        participants: "",
+        additional: "",
+        destination: "",
+    })
+
+    const changeHandler = (e) =>{
+        setTripDetails({
+            ...tripDetails,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const book_now = async () =>{
+        console.log(tripDetails);
+        try{
+            const addTripResponse = await axios.post('http://localhost:4001/api/bookdestination', tripDetails)
+
+            if(addTripResponse.data.success){
+                alert("Trip booked successfully")
+            }else{
+                alert("Failed to book trip")
+            }
+        }catch(error){
+            console.log('An error occured', error);
+            alert("Failed to book trip")
+        }
+    }
+    return(
+        <Container>
+            <div className="form">
+                <div className="form-left">
+                    <h1>Create your travel plan right now</h1>
+                </div>
+                <div className="form-right">
+                    <div>
+                        <label className="block mb-2">Name</label>
+                        <input value={tripDetails.name} onChange={changeHandler} name="name" required type="text" placeholder="Enter Your Full Name" className="w-full border border-zinc-300 rounded p-2 mb-4" />
+                    </div>
+
+                    
+                    <div>
+                        <label className="block mb-2">Email</label>
+                        <input value={tripDetails.emailaddress} onChange={changeHandler} name="emailaddress" required type="text" placeholder="Enter Your email address" className="w-full border border-zinc-300 rounded p-2 mb-4" />
+                    </div>
+
+                    
+                    <div>
+                        <label className="block mb-2">Phone number</label>
+                        <input value={tripDetails.phonenumber} onChange={changeHandler} name="phonenumber" required type="tel" placeholder="+254 123 435 789" className="w-full border border-zinc-300 rounded p-2 mb-4" />
+                    </div>
+
+                    <div>
+                        <label className="block mb-2">Address</label>
+                        <input value={tripDetails.address} onChange={changeHandler} name="address" required type="text" placeholder="Enter Your address info" className="w-full border border-zinc-300 rounded p-2 mb-4" />
+                    </div>
+
+                    <div>
+                        <label className="block mb-2">City/State</label>
+                        <input value={tripDetails.city} onChange={changeHandler} name="city" required type="text" placeholder="Enter city,state or province" className="w-full border border-zinc-300 rounded p-2 mb-4" />
+                    </div>
+
+                    <div>
+                        <label className="block mb-2">Nationaility</label>
+                        <input value={tripDetails.nationality} onChange={changeHandler} name="nationality" required type="text" placeholder="E.g kenyan" className="w-full border border-zinc-300 rounded p-2 mb-4" />
+                    </div>
+
+                    <div>
+                        <label className="block mb-2">Select destination</label>
+                        <select value={tripDetails.destination} onChange={changeHandler} name="destination" required className="w-full border border-zinc-300 rounded p-2 mb-4">
+                            <option value="n/a"></option>
+                            <option value="Kenya">Kenya</option>
+                            <option value="Ethopia">Ethopia</option>
+                            <option value="Uganda">Uganda</option>
+                            <option value="Rwanda">Rwanda</option>
+                            <option value="Burundi">Burundi</option>
+                            <option value="Tanzania">Tanzania</option>
+                            <option value="Zambia">Zambia</option>
+                            <option value="Angola">Angola</option>
+                            <option value="Bostwana">Bostwana</option>
+                            <option value="Zimbabwe">Zimbabwe</option>
+                        </select>
+                    </div>
+
+                    <div>
+                        <label className="block mb-2">Tourist attration</label>
+                        <select value={tripDetails.touristattraction} onChange={changeHandler} name="touristattraction" required className="w-full border border-zinc-300 rounded p-2 mb-4">
+                            <option value="n/a"></option>
+                            <option value="Mt Kenya">Mt Kenya</option>
+                            <option value="Maasai Mara">Maasai Mara</option>
+                            <option value="Lake Nakuru">Lake Nakuru</option>
+                            <option value="Mombasa">Mombasa</option>
+                            <option value="Lake Victoria">Lake Victoria</option>
+                        </select>
+                    </div>
+
+                    <div>
+                        <label className="block mb-2">Activities</label>
+                        <select value={tripDetails.activities} onChange={changeHandler} name="activities" required className="w-full border border-zinc-300 rounded p-2 mb-4">
+                            <option value="n/a"></option>
+                            <option value="Activities 1">Activities 1</option>
+                            <option value="Activities 2">Activities 2</option>
+                            <option value="Activities 3">Activities 3</option>
+                            <option value="Activities 4">Activities 4</option>
+                            <option value="Activities 5">Activities 5</option>
+                        </select>
+                    </div>
+
+                    <div>
+                        <label className="block mb-2">Hotel and Lounges</label>
+                        <select value={tripDetails.hotel} onChange={changeHandler} name="hotel" required className="w-full border border-zinc-300 rounded p-2 mb-4">  
+                            <option value="n/a"></option>
+                            <option value="Hotel 1">Hotel 1</option>
+                            <option value="Hotel 2">Hotel 2</option>
+                            <option value="Hotel 3">Hotel 3</option>
+                            <option value="Hotel 4">Hotel 4</option>
+                        </select>
+                    </div>
+
+                    <div>
+                        <label className="block mb-2">Intended Month of Travel</label>
+                        <select value={tripDetails.month} onChange={changeHandler} name="month" required className="w-full border border-zinc-300 rounded p-2 mb-4">
+                            <option value="n/a"></option>
+                            <option value="January">January</option>
+                            <option value="February">February</option>
+                            <option value="March">March</option>
+                            <option value="April">April</option>
+                            <option value="May">May</option>
+                            <option value="June">June</option>
+                            <option value="July">July</option>
+                            <option value="August">August</option>
+                            <option value="September">September</option>
+                            <option value="October">October</option>
+                            <option value="November">November</option>
+                            <option value="December">December</option>
+
+                        </select>
+                    </div>
+
+                    <div>
+                        <label className="block mb-2">Intended Year of Travel</label>
+                        <select value={tripDetails.year} onChange={changeHandler} name="year" required className="w-full border border-zinc-300 rounded p-2 mb-4">
+                            <option value="n/a"></option>
+                            <option value="2024">2024</option>
+                            <option value="2025">2025</option>
+                        </select>
+                    </div>
+
+                    <div>
+                        <label className="block mb-2">Number of Days</label>
+                        <input value={tripDetails.days} onChange={changeHandler} name="days" required type="number" placeholder="How many days is the trip to take" className="w-full border border-zinc-300 rounded p-2 mb-4" />
+                    </div>
+
+                    <div>
+                        <label className="block mb-2">Tour Category</label>
+                        <select value={tripDetails.tourcategory} onChange={changeHandler} name="tourcategory" required className="w-full border border-zinc-300 rounded p-2 mb-4">
+                            <option value="n/a"></option>
+                            <option value="Solo travel">Solo travel</option>
+                            <option value="Group travel">Group travel</option>
+                            <option value="Honeymoon">Honeymoon</option>
+                            <option value="Family Travel">Family Travel</option>
+                        </select>
+                    </div>
+
+                    <div>
+                        <label className="block mb-2">Number of Participants</label>
+                        <input value={tripDetails.participants} onChange={changeHandler} name="participants" required type="number" placeholder="Enter Total number of people travelling" className="w-full border border-zinc-300 rounded p-2 mb-4" />
+                    </div>
+                    <div className="mb-6">
+                        <p className="text-lg font-medium mb-2">Description</p>
+                        <textarea value={tripDetails.additional} onChange={changeHandler} name="additional" required  placeholder="Enter any additional information you feel is left out" className="w-full border border-zinc-300 rounded p-2 mb-4"></textarea>
+                    </div>
+                    
+                </div>
+                
+            </div>
+            <div style={{ position: 'relative' }}>
+                <button onClick={book_now} className="bg-zinc-500 text-white py-2 px-4 rounded book" style={{ position: 'absolute', right: 880 }}>Submit</button>
+            </div>
+           
+        </Container>
+    )
+}
+
+const Container = styled.div`
+    width: 100%;
+    max-width: 1700px;
+    margin: 200px auto;
+    .form{
+      
+        display: grid;
+        grid-template-columns: 1.5fr 2fr;
+        align-items: center;
+    }
+    .form-left{
+        h1{
+            font-size: 2.5rem;
+            font-weight: 700;
+            margin-bottom: 20px;
+        }
+    }
+    .form-right{
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        gap: 10px;
+    }
+    .book{
+
+    }
+`;
+export default Itinerary
